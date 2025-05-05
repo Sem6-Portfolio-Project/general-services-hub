94386d9844f530f991f07f85337416e385683ca5
Neranjan Pushpakumara <neranjan@aayutechnologies.com>
2025-05-05 21:45:27 +0530

    set the initial setup

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+
+export const BASE_URLS : string[] = ['http://localhost:8081','http://*'];
+
+export const ERROR_MESSAGES = {
+  UNEXPECTED_PROCESSING_ERROR: 'Unexpected processing error'
+}
---
+++ src/lambda/api/notification.ts
@@ -0,0 +1,25 @@
+import "reflect-metadata";
+import { Express } from "express";
+import { container } from "tsyringe";
+import serverless from "serverless-http";
+import { getApp } from "../../lib/express-app.js";
+import {
+    APIGatewayProxyEvent,
+    APIGatewayProxyHandler,
+    APIGatewayProxyResult,
+    Context
+} from 'aws-lambda';
+
+
+const app: Express = getApp();
+
+app.use('/notification', container.resolve(BusRouter).getRoutes());
+
+const _handler: serverless.Handler = serverless(app);
+
+export const handler: APIGatewayProxyHandler = async (
+        event: APIGatewayProxyEvent,
+        context: Context
+): Promise<APIGatewayProxyResult> =>{
+    return (await _handler(event,context)) as APIGatewayProxyResult;
+}
